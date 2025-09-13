9a01ae42db47d9b1154fa7bb2df9e744c3235c41
Tushar Kumar <tkgoyal104@gmail.com>
2025-09-13 19:15:06 +0530

    PopularPackages Updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
-import Navbar from '../components/Navbar'
-import HeroSection from '../components/HeroSection'
-import FeaturedDestinations from '../components/FeaturedDestinations'
-import WeatherSection from '../components/WeatherSection'
-import PopularPackages from '../components/PopularPackages'
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+import Navbar from '../components/Navbar';
+import HeroSection from '../components/HeroSection';
+import FeaturedDestinations from '../components/FeaturedDestinations';
+import WeatherSection from '../components/WeatherSection';
+import PopularPackages from '../components/PopularPackages';
+import { useEffect } from "react";
 
 export default function Home(){
+  useEffect(() => {
+    AOS.init({duration: 1000, once: true})
+  })
     return(
         <>
           <Navbar/>
