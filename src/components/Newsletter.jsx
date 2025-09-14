a4d211ac95db24ede1f1c9c37ebdc21997fd1e10
Tushar Kumar <tkgoyal104@gmail.com>
2025-09-14 11:08:53 +0530

    Newsletter Updated

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Newsletter.css'
+
+export default function Newsletter() {
+  return (
+    <section className='newsletter-section' id='newsletter'>
+        <div className='newsletter-container' data-aos='fade-up'>
+            <h2 className='newsletter-title'>
+                Stay Updated with Travel Explorer
+            </h2>
+            <p className='newsletter-text'>
+                Subscribe to get the latest travel deals, tips, and destination ideas directly to your inbox.
+            </p>
+
+            <form className="">
+                <input 
+                    type="email" 
+                    placeholder='Enter your email'
+                    className="newsletter-input" 
+                    required 
+                />
+                <button type='submit' className='newsletter-btn'>Subscribe</button>
+            </form>
+        </div>
+    </section>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import FeaturedDestinations from '../components/FeaturedDestinations';
 import WeatherSection from '../components/WeatherSection';
 import PopularPackages from '../components/PopularPackages';
 import Testimonials from "../components/Testimonials";
+import Newsletter from "../components/Newsletter";
 import { useEffect } from "react";
 
 export default function Home(){
@@ -21,6 +22,7 @@ export default function Home(){
           <WeatherSection/>
           <PopularPackages/>
           <Testimonials/>
+          <Newsletter/>
         </>
     )
 }
